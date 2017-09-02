de9c11d1b9046225be0644c306c175b46385c293
Thomas <tmcothran@hotmail.com>
2017-09-02 16:08:52 -0400

    Mostly works.

--- tictactoe.py
+++ tictactoe.py
@@ -3,44 +3,73 @@ import os
 import random
 import time
 import copy
+import re
 
 import computer_moves
 
 
 playerTypes = ['x', 'o']
-wrongInputMessage = ('I do not understand. ' +
-                     'To make a move, type the number of the row, then the ' +
-                     'column. For example, "1 2". Type "q" to quit.')
+wrongInputMessage = (
+    'I do not understand. ' +
+    'To make a move, type the character for the row, then the number for ' +
+    'the Row. For example, "A 2". Type "q" to quit.'
+)
 isTied = lambda g: len(computer_moves.allOpenSlots(g)) < 1
 
 
-def welcome_message(playerType):
-    if playerType not in playerTypes:
-        raise ValueError
-    return (
-        "Welcome to tic-tac-toe. To make a move, type in the  " +
-        "row and column numbers. For instance, to select the " +
-        "second column on the first row, type 2 1 \n" +
-        "To quit, type q. \n You are player {}".format(playerType)
-    )
+welcome_message = (
+    "Welcome to tic-tac-toe. To make a move, type in the  " +
+    "row and column numbers. For instance, to select the " +
+    "second column on the first row, type 2 1\n" +
+    "To quit, type q.\n"
+)
 
 
 def cleanInput(i):
-    cleanedArr = [int(x) for x in i if x in ['1', '2', '3']]
+    toNum = lambda n: {"A": 1, "B": 2, "C": 3}[n]
+    cleanedArr = re.sub(r'\s+', '', i)
     if len(cleanedArr) != 2:
         raise ValueError()
+    elif cleanedArr[0].lower() not in ['a', 'b', 'c']:
+        raise ValueError()
+    elif int(cleanedArr[1]) not in [1, 2, 3]:
+        raise ValueError()
     else:
-        return {'row': cleanedArr[0], 'column': cleanedArr[1]}
+        return {
+            'column': toNum(cleanedArr[0]),
+            'row': int(cleanedArr[1])  # For zero-indexing
+        }
 
 
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def drawBoard(boardState):
+    """Draws the tic tac toe board."""
+    headerRow = "     A   B   C\n"
+    breakLine = "   +---+---+---+"
+    lines = [headerRow, breakLine]
+
+    def colVals(n):
+        rawValues = [boardState[n - 1][i] for i in range(3)]
+        # Need to replace None values with spaces
+        replaceNones = lambda x: x if x is not None else ' '
+        displayVals = [replaceNones(x) for x in rawValues]
+        return displayVals
+
+    for n in range(1, 4):
+        line = '{}  | {} | {} | {} |'.format(n, *colVals(n))
+        lines.append(line)
+        lines.append(breakLine)
+    return lines
+
+
 def drawScreen(messages, game, print=print):
+    """Draw the tic tac toe board, any messages and the prompt."""
     clearScreen()
     if game:
-        print(game.draw())
+        [print(ln) for ln in drawBoard(game.state())]
         print(' ' * 5)
     if messages:
         for message in messages:
@@ -48,6 +77,7 @@ def drawScreen(messages, game, print=print):
 
 
 def handleMove(coordinates, game, playerType, message_queue):
+    """A wrapper for the makeMove method."""
     try:
         game.makeMove(
             coordinates['row'],
@@ -59,20 +89,13 @@ def handleMove(coordinates, game, playerType, message_queue):
         return game
 
 
-def computerMove(game):
-    return game.makeMove(3, 3, game.computerPlayer)
-
-
 class GameState:
     """A class to represent the state of our game and handle changes."""
 
-    def __init__(self, nextMove='x'):
-        """
-        self.human = random.choice(playerTypes)
+    def __init__(self, humanPlayer, nextMove='x'):
+        assert humanPlayer.lower() in playerTypes
+        self.human = humanPlayer.lower()
         self.computerPlayer = 'x' if self.human != 'x' else 'o'
-        """
-        self.human = 'x'
-        self.computerPlayer = 'o'
         self.nextMove = nextMove
         self.rows = [
             [None, None, None],
@@ -89,24 +112,17 @@ class GameState:
         return self
 
     def makeMove(self, row, col, playerType):
-        if self.rows[row - 1][col - 1] or row > 3 or col > 3:
+        # Zero index rows and cols
+        rowAdjusted, colAdjusted = row - 1, col - 1
+        if self.rows[rowAdjusted][colAdjusted] or row > 3 or col > 3:
             raise ValueError
-
-        self.rows[row - 1][col - 1] = playerType
+        self.rows[rowAdjusted][colAdjusted] = playerType
         self.toggleNextMove()
         return self
 
-    def draw(self):
-        formatRow = lambda r: "+ {} {} {} +".format(*r)
-        repNones = lambda sArr: [' ' if not s else s for s in sArr]
-        rowArr = [formatRow(repNones(r)) for r in self.rows]
-        table = '\n'.join(rowArr)
-        return table
 
-
-if __name__ == '__main__':
-    game = GameState()
-    message_queue = [welcome_message(game.human)]
+def runGame(game, message_queue=[]):
+    """Main game loop."""
     while True:
         if computer_moves.isWon(game.state()) or isTied(game.state()):
             message_queue = ['Game over!']
@@ -129,7 +145,24 @@ if __name__ == '__main__':
             handleMove(moveCoordinates, game, game.human, message_queue)
         else:
             moveCoords = computer_moves.minmax(game.state(), game.nextMove)
-            moveCoords = {'row': moveCoords[0] + 1, 'column': moveCoords[1] + 1}
+            moveCoords = {
+                'row': moveCoords[0] + 1,
+                'column': moveCoords[1] + 1
+            }
             message_queue.append('Computer moved to %s' % moveCoords)
             handleMove(moveCoords, game, game.nextMove, message_queue)
             continue
+
+
+if __name__ == '__main__':
+    clearScreen()
+    while True:
+        uinput = input('Which player do you want to be? X or O?\n-> ')
+        if uinput.lower() in playerTypes:
+            break
+        else:
+            clearScreen()
+            print('You need to type "X" or "O" to select a player')
+    game = GameState(humanPlayer=uinput.lower())
+    message_queue = ["X goes first".format(uinput.upper())]
+    runGame(game, message_queue)
